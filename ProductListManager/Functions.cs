3bcd86f5d809b872b72ceb7452735a9699d81b7e
Narendran-GCP <narendran.anu@gmail.com>
2026-09-04 22:35:47 +0200

    Updated Level 4 Initial code

---
+++ ProductListManager/Functions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductListManager
+{
+    internal class Functions
+    {
+        public static void AddProduct()
+        {
+            List<string> product = new List<string>(); //Creating Empty List
+
+
+            while (true)
+            {
+                Console.Write("Enter Product: ");
+
+                string? data = Console.ReadLine();
+
+                if (data.Trim().ToLower() == "exit")
+                {
+                    break;
+                }
+
+                // Check if the data is already in the list
+                if (product.Contains(data))
+                {
+                    Console.WriteLine("WARNING:Product already exists.");
+                }
+                else
+                {
+                    product.Add(data);
+                    Console.WriteLine("Product added successfully.");
+                }
+
+            }
+        }
+
+        public static void ViewProduct()
+        {
+            //Console.WriteLine("Searching product...");
+            // View product logic here
+        }
+
+        public static void SearchProduct()
+        {
+            //Console.WriteLine("Searching product...");
+            // Search product logic here
+        }
+
+        public static void DeleteProduct()
+        {
+            //Console.WriteLine("Deleting product...");
+            // Delete product logic here
+        }
+
+        public static void Statistics()
+        {
+            
+            // Statistics logic here
+        }
+    }
+}
--- ProductListManager/Program.cs
+++ ProductListManager/Program.cs
@@ -74,4 +74,144 @@ foreach (string name in product)
 //-------------------------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------------------------------------------
 
+/*
 
+Console.WriteLine("-------------------------------");
+Console.WriteLine("PRODUCT LIST MANAGER - LEVEL 3");
+Console.WriteLine("-------------------------------");
+
+Console.WriteLine("Enter product names.\nType 'exit' to finish.");
+Console.WriteLine();
+
+List<string> product = new List<string>(); //Creating Empty List
+
+
+while (true)
+{
+    Console.Write("Product:");
+
+    string? data = Console.ReadLine();
+
+    if (data.Trim().ToLower() == "exit")
+    {
+        break;
+    }
+
+    if (string.IsNullOrWhiteSpace(data))
+    {
+        Console.WriteLine("ERROR: Input cannot be empty.");
+        continue;
+    }
+
+    if (!data.Contains("-"))
+    {
+        Console.WriteLine("ERROR: Product must contain a dash (-).");
+        continue;
+    }
+
+    //Checking Valid examples
+
+    string[] parts  = data.Split('-');
+    string leftside = parts[0];
+    string rightside = parts[1];
+
+    //Checking left side contains letters only
+
+    bool lettersOnly = true;
+
+    foreach (char character in leftside)
+
+    {
+
+        if (!char.IsLetter(character))
+        {
+            lettersOnly = false;
+            break;
+        }
+
+    }
+
+    if (!lettersOnly || leftside.Length == 0)
+    {
+        Console.WriteLine("ERROR: The left side must contain letters only.");
+        continue;
+    }
+
+    //Checking right side contains numbers only
+
+    if (!int.TryParse(rightside, out int number))
+    {
+        Console.WriteLine("ERROR: The right side must contain numbers only.");
+        continue;
+    }
+
+    if (number < 200 || number > 500)
+    {
+        Console.WriteLine("ERROR: The numeric part must be between 200 and 500.");
+        continue;
+    }
+
+  
+    product.Add(data); //Adding data to the List.
+
+}
+
+product.Sort(); //Sort products in ascending order
+
+Console.WriteLine();
+Console.WriteLine("Sorted valid products:");
+
+foreach (string name in product)
+{
+    Console.WriteLine(name); //Listing all user entered product names
+}
+
+*/
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
+using ProductListManager;
+Console.WriteLine("-------------------------------");
+Console.WriteLine("PRODUCT LIST MANAGER - LEVEL 4");
+Console.WriteLine("-------------------------------");
+
+
+Console.WriteLine("1. Add Product");
+Console.WriteLine("2. View Products");
+Console.WriteLine("3. Search Product");
+Console.WriteLine("4. Delete Product");
+Console.WriteLine("5. Statistics");
+Console.WriteLine("6. Exit");
+
+Console.WriteLine();
+
+Console.Write("Select option: ");
+
+string userInput = Console.ReadLine();
+switch (userInput)
+{
+    case "1":
+        //Console.Write("Enter product: ");
+        Functions.AddProduct();
+        break;
+    case "2":
+        Functions.ViewProduct();
+        break;
+    case "3":
+        
+        Functions.SearchProduct();
+        break;
+    case "4":
+        Functions.DeleteProduct();
+        break;
+    case "5":
+        Functions.Statistics();
+        break;
+    case "6":
+        Console.WriteLine("Saving products...");
+        Console.WriteLine("Application closed.");
+        return;
+    default:
+        Console.WriteLine("Invalid option.");
+        break;
+}
