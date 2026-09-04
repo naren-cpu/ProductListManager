3b8aa41cbdcf7d93cc99fa3d2a172b5d4d8c9bd4
Narendran-GCP <narendran.anu@gmail.com>
2026-09-04 10:11:51 +0200

    Level 1 Code

--- ProductListManager/Program.cs
+++ ProductListManager/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("-------------------------------");
+Console.WriteLine("PRODUCT LIST MANAGER - LEVEL 1");
+Console.WriteLine("-------------------------------");
+
+Console.WriteLine("Enter product names.\nType 'exit' to finish.");
+Console.WriteLine();
+
+List<string> product = new List<string>(); //Creating Empty List
+
+
+while(true)
+{
+    Console.Write("Product:");
+
+    string? data = Console.ReadLine();
+
+    if (data == "exit")
+    {
+        break;
+    }
+    product.Add(data); //Adding data to the List.
+    
+}
+
+Console.WriteLine(); //Empty Line
+Console.WriteLine("Products entered:");
+
+foreach(string name in product)
+{
+    Console.WriteLine(name); //Listing all user entered product names
+}
+
+
+
