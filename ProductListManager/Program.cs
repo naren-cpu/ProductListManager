7060ff266a04a909581053ce053f7746c60e03ba
Narendran-GCP <narendran.anu@gmail.com>
2026-09-04 11:04:00 +0200

    Updated with Level 2 Code.

--- ProductListManager/Program.cs
+++ ProductListManager/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 Console.WriteLine("-------------------------------");
 Console.WriteLine("PRODUCT LIST MANAGER - LEVEL 1");
 Console.WriteLine("-------------------------------");
@@ -23,7 +23,7 @@ while(true)
     
 }
 
-Console.WriteLine(); //Empty Line
+Console.WriteLine();
 Console.WriteLine("Products entered:");
 
 foreach(string name in product)
@@ -31,5 +31,47 @@ foreach(string name in product)
     Console.WriteLine(name); //Listing all user entered product names
 }
 
+*/
+//-------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
+/*
+
+Console.WriteLine("-------------------------------");
+Console.WriteLine("PRODUCT LIST MANAGER - LEVEL 2");
+Console.WriteLine("-------------------------------");
+
+Console.WriteLine("Enter product names.\nType 'exit' to finish.");
+Console.WriteLine();
+
+List<string> product = new List<string>(); //Creating Empty List
+
+
+while (true)
+{
+    Console.Write("Product:");
+
+    string? data = Console.ReadLine();
+
+    if(data.Trim().ToLower() == "exit")
+    {
+        break;
+    }
+    product.Add(data); //Adding data to the List.
+
+}
+
+product.Sort(); //Sort products in ascending order
+
+Console.WriteLine();
+Console.WriteLine("Sorted product list:");
+
+foreach (string name in product)
+{
+    Console.WriteLine(name); //Listing all user entered product names
+}
+
+*/
+//-------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
 
 
